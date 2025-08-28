ef9d818e42229a8c5861ccc77fba84c4a97c5987
junyaTsukamoto <br17058@shibaura-it.ac.jp>
2025-08-28 11:58:23 +0900

    初期ファイルの生成

---
+++ flight-map/src/api.js
@@ -0,0 +1,9 @@
+// flight-map/src/api.js
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:8000";
+
+export const endpoints = {
+    airports: `${API_BASE}/airports`,
+    flights: `${API_BASE}/flights`,
+};
+
+export default endpoints;
